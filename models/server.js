1b1414196f79bc4d115dc08f870ffb5a4e318f6d
Vladimir <mendozahum20@gmail.com>
2022-04-21 00:53:02 -0600

    feat: fix errors

--- models/server.js
+++ models/server.js
@@ -53,7 +53,7 @@ class Server {
 
         this.app.use( this.userPath, require('../routes/user.route'));
         this.app.use( this.authPath, require('../routes/auth.route'));
-        this.app.use( this.officePath, require('../routes/office.route'));
+        // this.app.use( this.officePath, require('../routes/office.route'));
         
     }
     
--- routes/user.route.js
+++ routes/user.route.js
@@ -24,7 +24,7 @@ const {
 
 router.get('/', userGet);
 
-router.get('/:role', [
+router.get('/filter/:role', [
 	check('role').custom( isRoleValido ),
 	validateFields
 ], userRoles);
