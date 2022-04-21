b6e2871b0de5c7ccea9ebe075d4ae2ba1b3919b3
Vladimir <mendozahum20@gmail.com>
2022-04-21 00:40:26 -0600

    feat: add patients model

---
+++ models/patients.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const PatientSchema = Schema({
+    fullName: {
+        type: String,
+        require: [ true, 'The fullName is required']
+    },
+    userId: {
+        required: true,
+        type: Schema.types.objectId,
+        ref: 'User',
+    },
+    dni: {
+        type: String,
+        require: [ true, 'The dni is required']
+    }, 
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        unique: true
+    },
+    insurance_name: {
+        type: String,
+        required: [true, 'The insurance_name is required']
+    }, 
+    insurance_number: {
+        type: String,
+        required: [true, 'The insurance_number is required']
+    },  
+    status: {
+        type: Boolean,
+        default: true
+    },
+});
+
+PatientSchema.methods.toJSON = function() {
+    const { __v, password, _id, ...patient } = this.toObject();
+
+    patient.id = _id;
+    return patient;
+}
+
+module.exports = model('Patient',  PatientSchema);
